a3b07d39dc8ea08712282c7ea45031430ee7a957
André G. Bottan <agbottan@gmail.com>
2017-10-20 18:15:07 -0200

    Função Empréstimo

--- Controllers/CarrinhoController.cs
+++ Controllers/CarrinhoController.cs
@@ -1,19 +1,25 @@
 using BibliotecaMVC.Data;
 using BibliotecaMVC.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace BibliotecaMVC.Controllers
 {
     public class CarrinhoController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public CarrinhoController(ApplicationDbContext context)
+        public CarrinhoController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
+            _userManager = userManager;
             _context = context;
         }
 
@@ -36,6 +42,48 @@ namespace BibliotecaMVC.Controllers
             listaLivros.Add(livro);
             SetCarrinho(listaLivros);
 
+            return View("Index", GetCarrinho());
+        }
+
+        // POST: EmprestarLivros
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EmprestarLivros()
+        {
+            // Verificamos se o usuário está logado
+            if (User.Identity.IsAuthenticated)
+            {
+
+                // Pegar ID do Usuário
+                var userID = _userManager.GetUserId(HttpContext.User);
+
+                // Criar empréstimo
+                Emprestimo emprestimo = new Emprestimo()
+                {
+                    ApplicationUserId = userID,
+                    DataInicio = DateTime.Now.ToString("dd/MM/yyyy"),
+                    DataFim = DateTime.Now.AddDays(7).ToString("dd/MM/yyyy"),
+                    UsuarioID = 1, // Fixo p/ não dar erro
+                    LivroEmprestimo = new List<LivroEmprestimo>()
+                };
+
+                // Resgatar lista de livros no carrinho
+                List<Livro> listaLivros = GetCarrinho();
+
+                // Inserir a lista de livros na tabela LivroEmprestimo
+                foreach (var item in listaLivros)
+                {
+                    LivroEmprestimo livroEmprestimo = new LivroEmprestimo();
+                    livroEmprestimo.LivroID = item.LivroID;
+                    livroEmprestimo.Emprestimo = emprestimo;
+
+                    emprestimo.LivroEmprestimo.Add(livroEmprestimo);
+                }
+
+                // Inserir o novo empréstimo na tabela
+                _context.Add(emprestimo);
+                await _context.SaveChangesAsync();
+            }
             return View("Index", GetCarrinho());
         }
 
